8276d92691ce01ec17fa490ad57c36ece70c5cf7
Lesena13 <kravchenkol@i.ua>
2016-10-01 23:19:20 +0300

    Add files via upload

---
+++ src/dist/file.main.js
@@ -0,0 +1,14 @@
+console.log( 'Lena' );
+/*(function() {
+	var tree = 'oak';
+ 
+	var funcOne = function () {};
+ 
+	console.log( tree );
+})();*/		;(function() {
+	var two = 2;
+	var funcTwo = function () {
+	};
+ 
+	console.log( two );
+})();			
---
+++ src/js/file1.js
@@ -0,0 +1,8 @@
+console.log( 'Lena' );
+/*(function() {
+	var tree = 'oak';
+ 
+	var funcOne = function () {};
+ 
+	console.log( tree );
+})();*/		
