597f64669c3b2ae25ba229ce96f3c9eaaf2e75f9
Lesena13 <kravchenkol@i.ua>
2016-10-04 22:02:24 +0300

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,128 @@
+//Стандартный экспорт модуля в nodejs
+module.exports = function(grunt) {
+var mapValues = require('lodash.mapvalues');
+
+  // Инициализация конфига GruntJS
+  grunt.initConfig({
+     //таск для конвертации less в css
+        less: {
+            development: {
+                options: {
+                    //указать, минифицировать ли файл
+                    compress: false
+                },
+                files: {
+                    //тут соответственно указать путь где создавать
+                    //конвертируемый файл
+                    "src/sass/stylemain.css": "src/less/stylemain.less"
+                }
+            }
+        },
+    //Склеивание JS файлов
+    concat: {
+        main: { src: ['src/js/mysimpleslider.js', 'src/js/slider.js'], dest: 'src/js/scripts.js' }
+    },
+
+    // Сжатие общего JS файла
+    uglify: {
+       options: {
+                //нужно ли минифицировать (false) или просто склеить (true)
+                beautify: false
+            },
+            my_target: {
+                files: {
+                        //файл на выходе будет main.js, а файлы на входе указываем в
+                        //массиве по порядку.
+                        'src/js/scripts.js': [
+                        'src/js/sliderjs',
+                        'src/js/mysimpleslider.js'
+                    ]
+                }
+            }
+    },
+
+    // SASS
+    sass: {
+      dist: {
+        files: [{
+		   expand: true,
+		   cwd: 'src/css',
+		   src: ['*.scss'],
+		   dest: 'src/sass',
+		   ext: 'main.css'
+        }]
+      }
+    },
+    
+    // CSS Min
+    cssmin: {
+         minify: {
+    files: [{
+      expand: true,
+      cwd: 'src/css/',
+      src: ['*.css', '!*.min.css'],
+      dest: 'src/sass/',
+      ext: '.css'
+    }, {
+      expand: true,
+      cwd: 'src/css/',
+      src: ['*.min.css'],
+      dest: 'src/sass/',
+      ext: '.min.css'
+    }]
+  }
+        },
+     //этот таск из серии postcss, те теперь не нужно будет сразу писать префиксы в css
+        //autoprefixer сделает это сам, инфу берет с caniuse.com
+        autoprefixer: {
+                //указывам файл в котором нужно проставить префиксы, он сам его перезапишит
+                no_dest: {
+                    src: "src/sass/stylemain.css"
+                }
+            },
+    watch: {
+        options: {
+        livereload: true,
+      },
+      scripts: {
+            files: ['js/*.js'],
+            tasks: ['concat', 'uglify'],
+            options: {
+                spawn: false
+            }
+        },
+     css:{
+	         files: ['src/css/*.scss'],
+			 tasks: ['sass'],
+			 options: {
+            spawn: false,
+        },
+	   },
+	   //тут пишем за каким типом файлов следить
+            less: {
+                //тут указываем за какими файлами следить **/**/*.less - означает
+                //следить за всеми .less файлами, которые находятся на 2х уровнях
+                //вложенности от папки less/
+                files: ['src/sass/*.less'],
+                //какие таски надо выполнить при изменении файлов .less
+                tasks: ['less', 'autoprefixer']
+            }
+    }
+
+
+  });
+
+  // Загрузка модулей, которые предварительно установлены
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+   //указываем названия модулей необходимых для работы
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-lodash');
+  // Эти задания будут выполнятся сразу же когда вы в консоли напечатание grunt, и нажмете Enter
+  grunt.registerTask('default', ['concat', 'uglify', 'sass','cssmin','autoprefixer','less']);
+};
